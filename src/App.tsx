74e20891439d6109eb89ef4a9d129f7216f7889d
HungKungE <jonsun59@gamil.com>
2024-06-26 09:48:48 +0900

    [2-1] BoarList 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { useState } from 'react'
 import './App.css'
 import { appContainer, board, buttons } from './App.css.ts'
+import BoardList from './components/BoardList/BoardList.tsx'
 
 function App() {
+  const [activeBoardId, setActiveBoardId] = useState('board-0');
+
   return (
     <>
       <div className={appContainer}>
-        <div className={board}></div>
+        <div className={board}>
+          <BoardList activeBoardId={activeBoardId} setActiveBoardId={setActiveBoardId}/>
+        </div>
         <div className={buttons}>
           <button>이 게시판 삭제하기</button>
           <button></button>
