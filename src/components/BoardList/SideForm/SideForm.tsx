6149d3313bf28b9f92b0aec7a8420db5bc950d29
HungKungE <jonsun59@gmail.com>
2024-06-26 21:49:23 +0900

    [2-3] SideForm 생성

--- src/components/BoardList/SideForm/SideForm.tsx
+++ src/components/BoardList/SideForm/SideForm.tsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
+
 interface SideFormProps {
   setIsFormOpen : React.Dispatch<React.SetStateAction<boolean>>;
+  inputRef : React.RefObject<HTMLInputElement>;
 }
 
-const SideForm:React.FC<SideFormProps> = ({setIsFormOpen}) => {
+const SideForm:React.FC<SideFormProps> = ({setIsFormOpen, inputRef}) => {
+
+  const [inputText, setInputText] = useState('');
+
   return (
-    <div onClick={()=>setIsFormOpen}>SideForm</div>
+    <div>
+      <input
+        // ref={inputRef}
+        autoFocus
+        type='text'
+        placeholder='새로운 게시판 등록하기'
+        value={inputText}
+        onChange={e=>setInputText(e.target.value)}
+        onBlur={()=>setIsFormOpen(false)}
+      ></input>
+    </div>
   )
 }
 
