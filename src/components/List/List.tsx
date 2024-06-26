3243a4aacbe1034ef32afb5795a9eff49164d50a
HungKungE <jonsun59@gamil.com>
2024-06-26 23:03:52 +0900

    [2-5] List Container

---
+++ src/components/List/List.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const List = ({list, boardId}) => {
+  return (
+    <div>List</div>
+  )
+}
+
+export default List
---
+++ src/components/ListsContainer/ListsContainer.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { IList } from '../../types'
+import List from '../List/List'
+import ActionButton from '../ActionButton/ActionButton'
+import { listsContainer } from './ListsContainer.css'
+
+interface ListsContainerProps {
+    lists : IList[],
+    boardId : string
+}
+
+const ListsContainer: React.FC<ListsContainerProps> = ({lists, boardId}) => {
+
+    const renderLists = () => {
+        return lists.map(list => {
+                return <List key={list.listId} list={list} boardId={boardId}/>
+        })
+    }
+
+    return (
+        <div className={listsContainer}>
+            {renderLists()}
+            <ActionButton/>
+        </div>
+    )
+}
+
+export default ListsContainer
