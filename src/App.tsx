d6f441b50e0e6705eaf78f60c7b7fe9093700462
HungKungE <jonsun59@gmail.com>
2024-06-26 23:03:52 +0900

    [2-5] List Container

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,21 @@ import { useState } from 'react'
 import './App.css'
 import { appContainer, board, buttons } from './App.css.ts'
 import BoardList from './components/BoardList/BoardList.tsx'
+import ListsContainer from './components/ListsContainer/ListsContainer.tsx';
+import { useTypedSelector } from './hooks/redux.ts';
 
 function App() {
   const [activeBoardId, setActiveBoardId] = useState('board-0');
+  const boards = useTypedSelector(state=>state.boards.boardArray);
+  const getActiveBoard = boards.filter(board => board.boardId === activeBoardId)[0];
+  const lists = getActiveBoard.lists;
 
   return (
     <>
       <div className={appContainer}>
         <BoardList activeBoardId={activeBoardId} setActiveBoardId={setActiveBoardId}/>
         <div className={board}>
+          <ListsContainer lists={lists} boardId={getActiveBoard.boardId}/>
         </div>
         <div className={buttons}>
           <button>이 게시판 삭제하기</button>
---
+++ src/components/ListsContainer/ListsContainer.css.ts
@@ -0,0 +1,11 @@
+import { style } from "@vanilla-extract/css";
+import { vars } from "../../App.css";
+
+
+export const listsContainer = style({
+    height : 'max-content',
+    display : 'flex',
+    flexWrap : 'wrap',
+    rowGap : vars.spacing.listSpacing,
+    margin : vars.spacing.listSpacing
+});
