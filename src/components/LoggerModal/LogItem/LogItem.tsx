9ea2cd24b2f63dc33ee9c641a0e9a964911eb098
HungKungE <jonsun59@gmail.com>
2024-06-28 01:37:03 +0900

    [3-5] LogItem component 추가

--- src/components/LoggerModal/LogItem/LogItem.tsx
+++ src/components/LoggerModal/LogItem/LogItem.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { ILogItem } from '../../../types'
+import { BsFillPersonFill } from 'react-icons/bs'
 
-const LogItem = ({logItem}) => {
+interface LogItemProps {
+  logItem : ILogItem
+}
+
+const LogItem : React.FC<LogItemProps> = ({logItem}) => {
+  const timeOffset = new Date(Date.now() - Number(logItem.logTimeStamp));
+
+  const showOffsetTime = `
+    ${ timeOffset.getMinutes() > 0 ? `${timeOffset.getMinutes()}m` : ""}
+    ${ timeOffset.getSeconds() > 0 ? `${timeOffset.getSeconds()}s ago` : ""}
+    ${ timeOffset.getSeconds()=== 0 ? `just now` : ""}
+  `;
+
   return (
-    <div>LogItem</div>
+    <div>
+      <div>
+        <BsFillPersonFill />
+        {logItem.logAuthor}
+      </div>
+      <div>{logItem.logMessage}</div>
+      <div>{showOffsetTime}</div>
+    </div>
   )
 }
 
